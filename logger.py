e850483c49a19436e06966edafa1ff225d690e72
Xuehao Zhou <47078436+xuehao-marker@users.noreply.github.com>
2021-09-24 16:10:19 +0800

    Update logger.py

--- logger.py
+++ logger.py
@@ -9,15 +9,19 @@ class Tacotron2Logger(SummaryWriter):
     def __init__(self, logdir):
         super(Tacotron2Logger, self).__init__(logdir)
 
-    def log_training(self, reduced_loss, grad_norm, learning_rate, duration,
+    def log_training(self, mel_loss, gate_loss, reduced_loss, grad_norm, learning_rate, duration,
                      iteration):
-            self.add_scalar("training.loss", reduced_loss, iteration)
+            self.add_scalar("mel.loss", mel_loss, iteration)
+            self.add_scalar("gate.loss", gate_loss, iteration)
+            self.add_scalar("total.loss", reduced_loss, iteration)
             self.add_scalar("grad.norm", grad_norm, iteration)
             self.add_scalar("learning.rate", learning_rate, iteration)
             self.add_scalar("duration", duration, iteration)
 
-    def log_validation(self, reduced_loss, model, y, y_pred, iteration):
-        self.add_scalar("validation.loss", reduced_loss, iteration)
+    def log_validation(self, mel_loss, gate_loss, reduced_loss, model, y, y_pred, iteration):
+        self.add_scalar("mel.loss", mel_loss, iteration)
+        self.add_scalar("gate.loss", gate_loss, iteration)
+        self.add_scalar("total.loss", reduced_loss, iteration)
         _, mel_outputs, gate_outputs, alignments = y_pred
         mel_targets, gate_targets = y
 
